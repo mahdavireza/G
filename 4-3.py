6ed61cd32152f5167d730b351b67de854a160193
mahdavireza <146663499+mahdavireza@users.noreply.github.com>
2023-10-31 11:52:13 -0700

    Add files via upload

---
+++ 4-3.py
@@ -0,0 +1,29 @@
+def factorial(n):
+    result = 1
+    for i in range(1, n+1):
+        result *= i
+    return result
+
+n = 3
+factorial_result = factorial(n)
+print(factorial_result)
+
+def factorial(n):
+    result = 1
+    for i in range(1, n+1):
+        result *= i
+    return result
+
+n = 2
+factorial_result = factorial(n)
+print(factorial_result)
+
+def factorial(n):
+    result = 1
+    for i in range(1, n+1):
+        result *= i
+    return result
+
+n = 4
+factorial_result = factorial(n)
+print(factorial_result)
